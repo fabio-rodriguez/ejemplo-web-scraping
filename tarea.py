0b5571a4cb451362daf2c79618e068ed96feebfe
fragnarxx <fabio25.rodriguez@gmail.com>
2021-04-08 16:48:06 +0200

    termine el proyecto

---
+++ tarea.py
@@ -0,0 +1,26 @@
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import re
+
+ua = UserAgent()
+headers = {'user-agent':ua.chrome}
+url = 'https://boston.craigslist.org/search/sof'
+
+page = requests.get(url, headers = headers)
+data = page.content
+
+soup = BeautifulSoup(data, 'lxml')
+
+#ofertas = soup.find('ul',class_='rows')
+lista_ofertas = soup.find_all('div', class_='result-info')
+
+for lista in lista_ofertas:
+    a = lista.find('a')
+    texto_oferta = a.text
+    url_oferta = lista.find('href')
+    url_oferta = a['href']
+    print('Title: ',texto_oferta)
+    print('URL: ', url_oferta)
+    print()
+
